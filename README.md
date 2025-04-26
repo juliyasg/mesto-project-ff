afde65d7714329c0e39917d14ee14d385507bc7a
Yuliya Gudinova <juliya.sg@yandex.kz>
2025-04-26 16:38:34 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/juliyasg/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,33 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector('.places__list');
+
+function createCard(data, cardDelete) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardNew = cardTemplate.querySelector('.card').cloneNode(true);
+
+  const cardImage = cardNew.querySelector('.card__image');
+  const cardTitle = cardNew.querySelector('.card__title');
+  const deleteButton = cardNew.querySelector('.card__delete-button');
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+
+  deleteButton.addEventListener('click', function() {
+    cardDelete(cardNew);
+  });
+
+  return cardNew;
+}
+
+function cardDelete(cardElement) {
+    cardElement.remove();
+}
+
+initialCards.forEach((item) => {
+  const cardNew = createCard(item, cardDelete);
+  placesList.append(cardNew);
+});
